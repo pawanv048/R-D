cc831f1fe06fb92a1834c880917e83665f551aa1
PAWAN VERMA <41580207+pawanv048@users.noreply.github.com>
2023-06-05 11:40:06 +0530

    Flatlist items proper spaceing

--- index.js
+++ index.js
@@ -1,54 +1,34 @@
-var myname = 20
-var myname = 30
-//console.log(myname);
-// re-decleard and re-assign is possible
 
-let num = 20
-num = 30
-// console.log(num)
-// Assinment of the value is possible
-//let num = 30 //Cannot redeclare block-scoped variable 'num'
+import {Dimensions, FlatList, View} from 'react-native';
 
-const value = 10;
-console.log(value);
-// neither re-declared nor assing with const 
-// declarations must be initialized with const
+const screenWidth = Dimensions.get('window').width;
+const numColumns = 2;
+const gap = 5;
 
+const availableSpace = screenWidth - (numColumns - 1) * gap;
+const itemSize = availableSpace / numColumns;
 
-// IN case of var
-// No block{}, scope is create, can be re-declared
-var count = 1
-var age = 15
-function sum(a,b){
-  var count = 0; 
-  return a+b;
-}
-if(age>18){
-  var count = 2;
-  console.log(count)
-}
+const renderItem = ({item}) => {
+  return (
+    <View
+      style={{
+        height: itemSize,
+        width: itemSize,
+        backgroundColor: 'pink',
+      }}
+    />
+  );
+};
 
-// in case of let 
-
-// all block {} have seperate scope 
-// only declare once in scope
-let counter = 1
-var age = 15
-function sum(a,b){
-  
-  counter = 0; 
-  return a+b;
-}
-if(age>18){
-  //let count = 2; // SyntaxError: Identifier 'count' has already been declared
-  //console.log(count)
-}
-
-// in case of const object and array you can change
-const person = {}
-person.name = 'amit'
-console.log(person.name)
-
-const cities = []
-cities.push('Punjab')
-console.log(cities)
+const RNTesterApp = props => {
+  return (
+    <FlatList
+      renderItem={renderItem}
+      data={new Array(100).fill(0)}
+      numColumns={numColumns}
+      contentContainerStyle={{gap}}
+      columnWrapperStyle={{gap}}
+      key={numColumns}
+    />
+  );
+};
